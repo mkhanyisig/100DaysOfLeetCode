e94fb5215ae34f1bcce868ed2f2dbbadd12178cb
Mkhanyisi Gamedze <mggamedz@colby.edu>
2020-02-12 22:17:44 -0500

    Add files via upload

---
+++ Day 5 - Contains Duplicates II.java
@@ -0,0 +1,44 @@
+/*
+     Day 4: Leetcode 217. Contains Duplicate
+     Time Spent: 77 mins
+     
+     Personal notes: Made deadly mistake of misnaming variable and that delayed my debugging, took long to catch
+*/
+
+
+
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> arr    = new HashMap<Integer,Integer>();
+        ArrayList<Integer> intarr = new ArrayList<Integer>(); // repeated indices
+        //Set<Integer> hs    = new HashSet<Integer>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(arr.containsKey(nums[i])){
+                if(!intarr.contains(arr.get(nums[i]))){
+                    
+                }
+                else{
+                    intarr.add(i);    
+                }
+            }  
+            else{
+                arr.put(nums[i],i);
+                intarr.add(i);
+                //hs.add(nums[i]);
+            }
+        }
+        System.out.println(" intarr size: "+intarr.size());
+        for(int i=0;i<intarr.size();i++){
+            for(int j=0;j<intarr.size();j++){
+                if(nums[i]==nums[j] && Math.abs(i-j)<=k && i!=j){
+                    System.out.println(nums[i]+"   k:"+k+"  diff: "+Math.abs(i-j) );
+                    return true;
+                }
+            }
+        }
+        return false;                             
+    } 
+}
