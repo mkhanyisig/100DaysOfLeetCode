9e722dfaa49117bc1a0e4d6deb92d65c87696cd7
Mkhanyisi Gamedze <mggamedz@colby.edu>
2020-02-20 16:08:48 -0500

    Add files via upload

---
+++ Day 13 - First Bad Version.java
@@ -0,0 +1,37 @@
+/*
+     Day 13: Leetcode 278. First Bad Version
+     Time Spent: 23 mins
+     
+     Personal notes: Iterative solutions fails fr very big numbers, so go with binary search solution
+*/
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        System.out.println(" num :"+n);
+        
+        int l=1;
+        int i=n;
+        // binary search
+        while(l<i){
+            int mid=l+(i-l)/2;
+            if(isBadVersion(mid)){
+                // do nothing
+                i=mid;
+            }
+            else{
+                l=mid+1;
+            }
+            
+        }
+        
+        
+        return l;
+        
+      
+    }
+}
